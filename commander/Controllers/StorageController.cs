9d0746fd04bc3b7e5c919b0fb9ca2801bd362467
rudxde <kj4derEchte@gmail.com>
2018-04-03 21:37:11 +0200

    feat(storageController): implemetation of basic file operations
    dev/

--- commander/Controllers/StorageController.cs
+++ commander/Controllers/StorageController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
 
 namespace commander.Controllers
 {
@@ -14,6 +15,13 @@ namespace commander.Controllers
         [Route("{Type}")]
         public IActionResult GetAllEntrys([FromRoute]String Type)
         {
+            ensureNameToBeSave(Type);
+            ensureDirectory(Type);
+            List<string> Files = new List<string>();
+            foreach (String file in System.IO.Directory.GetFiles("data/" + Type))
+            {
+                Files.Add((new System.IO.FileInfo(file)).Name);
+            }
             return new OkResult();
         }
 
@@ -21,18 +29,24 @@ namespace commander.Controllers
         [Route("{Type}/{Entry}")]
         public IActionResult GetEntryData([FromRoute]String Type, [FromRoute]String Entry)
         {
-            return new OkResult();
+            ensureNameToBeSave(Entry);
+            ensureNameToBeSave(Type);
+            ensureDirectory(Type);
+            if (!System.IO.File.Exists(getFileName(Type, Entry)))
+            {
+                return new StatusCodeResult(404);
+            }
+            return Content(System.IO.File.ReadAllText(getFileName(Type, Entry)));
         }
+
         [HttpPost]
         [Route("{Type}/{Entry}")]
-        public IActionResult AddEntryData([FromRoute]String Type, [FromRoute]String Entry)
+        public IActionResult AddEntryData([FromRoute]String Type, [FromRoute]String Entry, [FromBody]String FileBody)
         {
-            return new OkResult();
-        }
-        [HttpPut]
-        [Route("{Type}/{Entry}")]
-        public IActionResult UpdateEntryData([FromRoute]String Type, [FromRoute]String Entry)
-        {
+            ensureNameToBeSave(Entry);
+            ensureNameToBeSave(Type);
+            ensureDirectory(Type);
+            System.IO.File.WriteAllText(getFileName(Type, Entry), FileBody);
             return new OkResult();
         }
 
@@ -40,7 +54,41 @@ namespace commander.Controllers
         [Route("{Type}/{Entry}")]
         public IActionResult DeleteEntryData([FromRoute]String Type, [FromRoute]String Entry)
         {
+            ensureNameToBeSave(Entry);
+            ensureNameToBeSave(Type);
+            ensureDirectory(Type);
+            if (!System.IO.File.Exists(getFileName(Type, Entry)))
+            {
+                return new StatusCodeResult(404);
+            }
+            System.IO.File.Delete(getFileName(Type, Entry));
             return new OkResult();
+        }
+
+        private Regex saveName = new Regex("[A-Za-z0-9\\.]+");
+        private void ensureNameToBeSave(String Name)
+        {
+            if (!saveName.Match(Name).Success)
+            {
+                throw new Exception($"\"${Name}\" does not match the pattern of \"[A-Za-z0-9]+\"");
+            }
+        }
+
+        private String getFileName(String Type, String Entry)
+        {
+            return $"data/${Type}/${Entry}.entry";
+        }
+
+        private void ensureDirectory(String directory)
+        {
+            if (!System.IO.Directory.Exists("data"))
+            {
+                System.IO.Directory.CreateDirectory("data");
+            }
+            if (!System.IO.Directory.Exists("data/" + directory))
+            {
+                System.IO.Directory.CreateDirectory("data/" + directory);
+            }
         }
     }
 }
