628f8a5822bfc64aade5be3d3d55c36f33223156
MichaelRedM <michael.rudolph@redmedical.de>
2018-05-08 23:16:11 +0200

    chore(datamodel): adapt code to Datamodel V3

--- front/src/app/core/interfaces/ICueble.ts
+++ front/src/app/core/interfaces/ICueble.ts
@@ -1,9 +1,13 @@
 import { IFixture } from './IFixture';
 import { ICue } from './ICue';
+import { CueList } from '../library/cueList';
 
 export interface ICueble {
   fixture: IFixture;
+  channelIndex: number;
+  ignoreFade: boolean;
 
   getValue(): number;
-  shine(parentCue: ICue): void;
+  shine(parentCue: ICue, parentCueList: CueList): void;
+  isOverride(): boolean;
 }
