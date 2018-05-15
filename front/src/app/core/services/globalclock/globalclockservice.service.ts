61aa1341dc2eaf4fd78ac4f728088921d0388cb8
stovemeerkat <marvin.heinzelmann@t-online.de>
2018-05-15 09:28:19 +0200

    feat(GlobalClockService): Implement methods start() and stop(). Add empty method tick() that will be called repeatedly.

--- front/src/app/core/services/globalclock/globalclockservice.service.ts
+++ front/src/app/core/services/globalclock/globalclockservice.service.ts
@@ -2,7 +2,20 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class GlobalclockserviceService {
+  private timer: NodeJS.Timer;
 
-  constructor() { }
+  constructor() {}
+
+  private tick(): void {
+    // TODO
+  }
+
+  public start(): void {
+    this.timer = setInterval(this.tick, 1);
+  }
+
+  public stop(): void {
+    clearInterval(this.timer);
+  }
 
 }
