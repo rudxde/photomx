7d9909a4bb548686819f01a409d48da85c29e420
stovemeerkat <marvin.heinzelmann@t-online.de>
2018-05-15 19:43:49 +0200

    feat(GlobalClockService): Implement tick()-method and update the constructor accordingly.

--- front/src/app/core/services/globalclock/globalclockservice.service.ts
+++ front/src/app/core/services/globalclock/globalclockservice.service.ts
@@ -1,17 +1,26 @@
 import { Injectable } from '@angular/core';
+import { FixtureService } from '../fixture/fixture.service';
+import { ArtnetService } from '../artnet/artnet.service';
 
 @Injectable()
 export class GlobalclockserviceService {
   private timer: NodeJS.Timer;
+  private fixtureService: FixtureService;
+  private artnetService: ArtnetService;
 
-  constructor() {}
+
+  constructor(fixtureService: FixtureService, artnetService: ArtnetService) {
+    this.fixtureService = fixtureService;
+    this.artnetService = artnetService;
+  }
 
   private tick(): void {
-    // TODO
+    this.fixtureService.tick();
+    this.artnetService.sendArtnet();
   }
 
   public start(): void {
-    this.timer = setInterval(this.tick, 1);
+    this.timer = setInterval(this.tick, 200);  // TODO: delay is currently quite arbitrary
   }
 
   public stop(): void {
